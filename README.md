539f0761a881788516a6f63dd3b6113448ce47ff
Jason Toy <jasontoy@gmail.com>
2016-02-11 17:36:41 -0600

    change version

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 ## tensorflow seq2seq translator
+
+to run:
+```
+source ~/tensorflow/bin/activate
+python translate.py --data_dir ~/data_sets/eng-fren/
+```
--- seq2seq_model.py
+++ seq2seq_model.py
@@ -28,8 +28,7 @@ import tensorflow as tf
 from tensorflow.models.rnn import rnn_cell
 from tensorflow.models.rnn import seq2seq
 
-#from tensorflow.models.rnn.translate import data_utils
-import data_utils
+from tensorflow.models.rnn.translate import data_utils
 
 
 class Seq2SeqModel(object):
@@ -136,19 +135,19 @@ class Seq2SeqModel(object):
     if forward_only:
       self.outputs, self.losses = seq2seq.model_with_buckets(
           self.encoder_inputs, self.decoder_inputs, targets,
-          self.target_weights, buckets, lambda x, y: seq2seq_f(x, y, True),
+          self.target_weights, buckets, self.target_vocab_size,
+          lambda x, y: seq2seq_f(x, y, True),
           softmax_loss_function=softmax_loss_function)
       # If we use output projection, we need to project outputs for decoding.
       if output_projection is not None:
         for b in xrange(len(buckets)):
-          self.outputs[b] = [
-              tf.matmul(output, output_projection[0]) + output_projection[1]
-              for output in self.outputs[b]
-          ]
+          self.outputs[b] = [tf.nn.xw_plus_b(output, output_projection[0],
+                                             output_projection[1])
+                             for output in self.outputs[b]]
     else:
       self.outputs, self.losses = seq2seq.model_with_buckets(
           self.encoder_inputs, self.decoder_inputs, targets,
-          self.target_weights, buckets,
+          self.target_weights, buckets, self.target_vocab_size,
           lambda x, y: seq2seq_f(x, y, False),
           softmax_loss_function=softmax_loss_function)
 
@@ -185,7 +184,7 @@ class Seq2SeqModel(object):
       average perplexity, and the outputs.
 
     Raises:
-      ValueError: if length of encoder_inputs, decoder_inputs, or
+      ValueError: if length of enconder_inputs, decoder_inputs, or
         target_weights disagrees with bucket size for the specified bucket_id.
     """
     # Check if the sizes match.
